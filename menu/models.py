56fbe6b533c7962c256dd2c16835bf68ce33b385
keegan <kzo5060@psu.edu>
2017-04-19 15:24:55 -0400

    implemented Item model under menu app

--- menu/models.py
+++ menu/models.py
@@ -1,9 +1,26 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
-from datetime import datetime
 
+class Item(models.Model):
+    FOOD_OR_DRINK_CHOICES = (
+        ('Food', 'Food'),
+        ('Drink', 'Drink')
+    )
+    name = models.CharField(max_length=100)
+    food_or_drink = models.CharField(max_length=5, choices=FOOD_OR_DRINK_CHOICES)
+    is_breakfast = models.BooleanField(inital=False)
+    is_lunch = models.BooleanField(initial=False)
+    promo_flag = models.BooleanField(initial=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    supplier = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
 
 class Order(models.Model):
     user_name = models.CharField(max_length=128)
