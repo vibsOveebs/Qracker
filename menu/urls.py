59f87d31b36f1024575219838e7fd8025f1a3fe5
keegan <kzo5060@psu.edu>
2017-04-23 16:36:39 -0400

    possibly not throwing error? attempt 3

--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from menu import views
 
 urlpatterns = [
-    url(r'^additme', views.add_menuitem, name='additem'),
+    url(r'^additem', views.add_menuitem, name='additem'),
     url(r'^tacobell', views.add_order_tacobell, name='tacobell'),
 ]
