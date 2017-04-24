cb6cb2bdc061ad197d828b57712438e228d6c1a3
keegan <kzo5060@psu.edu>
2017-04-24 14:03:31 -0400

    Separated Search view into two functions, one for Search and one for the Results

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from menu.forms import AddItemForm, BrowseForm, SearchForm
 from menu.models import Item
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.models import User
 
 
 def add_menu_item(request):
@@ -22,31 +20,34 @@ def add_menu_item(request):
 def success(request):
     return render(request, 'menu/success.html')
 
-#@user_passes_test(lambda u: Group.objects.get(name='customer') in u.groups.all())
-
 
 # search view
 def search(request):
-    #form = SearchForm()
+    form = SearchForm()
+    return render(request, 'menu/search.html', {'form': form})
 
-    if request.method == 'GET':
-        form = SearchForm(request.GET)
-        if form.is_valid():
-            searchstring = request.GET.get('searchstring')
-            food_or_drink = request.GET.get('food_or_drink')
-            is_breakfast = request.GET.get('is_breakfast')
-            is_lunch = request.GET.get('is_lunch')
 
-            results = Item.objects.filter(
-                food_or_drink__exact=food_or_drink
-            ).filter(
-                is_breakfast__exact=is_breakfast
-            ).filter(
-                is_lunch__exact=is_lunch
-            ).filter(
-                name__icontains=searchstring
-            )
-            return render(request, 'menu/results.html', results)
+# results view
+def results(request):
+    # get search parameters
+    searchstring = request.GET.get('searchstring')
+    food_or_drink = request.GET.get('food_or_drink')
+    is_breakfast = request.GET.get('is_breakfast')
+    is_lunch = request.GET.get('is_lunch')
+
+    # filter the Item set using search parameters
+    results = Item.objects.filter(
+        food_or_drink__exact=food_or_drink
+    ).filter(
+        is_breakfast__exact=is_breakfast
+    ).filter(
+        is_lunch__exact=is_lunch
+    ).filter(
+        name__icontains=searchstring
+    )
+
+    # render
+    return render(request, 'menu/results.html', results)
 
 
 def browse(request):
