4c7aee17400f1e612859fc3580e78f48f34d6a7a
keegan <kzo5060@psu.edu>
2017-04-26 16:33:56 -0400

    orders/views formatting

--- orders/views.py
+++ orders/views.py
@@ -1,51 +1,86 @@
 from django.shortcuts import render
 from orders.models import Transaction
 
-from qracker.views import index
 
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.models import User
-from django.db.models import Q
-
-from menu.views import success
-
-from datetime import datetime
-
+# open requests view
 def openrequests(request):
+
+    # initialize context dictionary
     context_dict = {}
 
-    transactions = Transaction.objects.filter(delivers__isnull=True)
+    # get unclaimed transactions
+    transactions = Transaction.objects.filter(
+        delivers__isnull=True)
+
+    # update context dictionary
     context_dict['transactions'] = transactions
 
+    # render
     return render(request, "orders/openrequests.html", context_dict)
 
+
+# Pickup item button view
 def pickupitem(request):
+
+    # if pickup item has been submitted
     if request.method == 'GET':
+
+        # get transaction id and print it
         transactionid = request.GET['transactionid']
         print(transactionid)
+
+        # get transaction and print it
         transaction = Transaction.objects.get(id=transactionid)
         print(transaction)
+
+        # set delivers to the current user
         transaction.delivers = request.user
+
+        # save the form in the database
         transaction.save()
+
+        # render success page
         return pickupsuccess(request)
 
+
+# pickup success view
 def pickupsuccess(request):
     return render(request, 'orders/pickupsuccess.html')
 
+
+# My Requests view
 def myrequests(request):
+
+    # initialize context dictionary and grab userid
     context_dict = {}
     userid = request.user.id
 
-    transactions = Transaction.objects.filter(initiates__exact=userid).filter(delivery_time__isnull=True)
+    # find transactions that matches that userid
+    transactions = Transaction.objects.filter(
+        initiates__exact=userid).filter(
+        delivery_time__isnull=True)
+
+    # put transactions into the context dictionary
     context_dict['transactions'] = transactions
 
+    #render
     return render(request, "orders/myrequests.html", context_dict)
 
+
+# My Deliveries view
 def mydeliveries(request):
+
+    # initialize context dictionary and grab userid
     context_dict = {}
     userid = request.user.id
 
-    transactions = Transaction.objects.filter(delivers__exact=userid).filter(delivery_time__isnull=True)
+    # find transactions that matches that userid
+    transactions = Transaction.objects.filter(
+        delivers__exact=userid).filter(
+        delivery_time__isnull=True)
+
+    # put transactions into the context dictionary
     context_dict['transactions'] = transactions
 
+    # render
     return render(request, "orders/mydeliveries.html", context_dict)
