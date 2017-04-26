960083487c3d5605c2e000527474d508cf72d6bc
keegan <kzo5060@psu.edu>
2017-04-26 16:23:45 -0400

    orders/urls formatting

--- orders/urls.py
+++ orders/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from orders import views
 
 urlpatterns = [
-    url(r'^openrequests', views.openrequests, name='openrequests'),
-    url(r'^pickupitem', views.pickupitem, name='pickupitem'),
     url(r'^myrequests', views.myrequests, name='myrequests'),
     url(r'^mydeliveries', views.mydeliveries, name='mydeliveries'),
+    url(r'^openrequests', views.openrequests, name='openrequests'),
+    url(r'^pickupitem', views.pickupitem, name='pickupitem'),
 ]
