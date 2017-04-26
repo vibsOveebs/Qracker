7fc2b9aa219949c8ec2f6f6b53bd8f11f554790a
keegan <kzo5060@psu.edu>
2017-04-26 16:38:05 -0400

    userprofile/urls formatting

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from userprofile import views
 
 urlpatterns = [
-    url(r'^register', views.register, name='register'),
     url(r'^login', views.user_login, name='login'),
     url(r'^logout', views.user_logout, name='logout'),
+    url(r'^register', views.register, name='register'),
     url(r'^viewprofile', views.view_current_profile, name='viewprofile'),
 ]
