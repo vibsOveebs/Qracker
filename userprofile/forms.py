9e49598c8c307be6fac891496c0f1c26889c92b5
keegan <kzo5060@psu.edu>
2017-04-26 16:36:22 -0400

    userprofile/forms formatting

--- userprofile/forms.py
+++ userprofile/forms.py
@@ -3,15 +3,23 @@ from django.contrib.auth.models import User
 from userprofile.models import UserProfile
 
 
+# User Form
 class UserForm(forms.ModelForm):
+
+    # make sure password isn't in plaintext
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
+
+        # only show username, email, and password fields
         fields = ('username', 'email', 'password')
 
 
+# User Profile Form
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
+
+        # only show location, phone number, and picture fields
         fields = ('location', 'phone_number', 'picture')
