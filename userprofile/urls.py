32ff64ecbcb8da4073e53f7044e773a4c75a622f
keegan <kzo5060@psu.edu>
2017-04-26 13:54:38 -0400

    updated userprofile/urls for view profile

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,5 +4,6 @@ from userprofile import views
 urlpatterns = [
     url(r'^register', views.register, name='register'),
     url(r'^login', views.user_login, name='login'),
-    url(r'^logout', views.user_logout, name='logout')
+    url(r'^logout', views.user_logout, name='logout'),
+    url(r'^viewprofile', views.view_current_profile, name='viewprofile'),
 ]
