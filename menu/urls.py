774423e18554b39d9852881d4f8a84703a554110
keegan <kzo5060@psu.edu>
2017-04-26 16:19:57 -0400

    menu/urls formatting

--- menu/urls.py
+++ menu/urls.py
@@ -3,9 +3,9 @@ from menu import views
 
 urlpatterns = [
     url(r'^additem', views.add_menu_item, name='additem'),
-    url(r'^search', views.search, name='search'),
-    url(r'^searchresults', views.searchresults, name='searchresults'),
     url(r'^browse', views.browse, name='browse'),
     url(r'^browseresults', views.browseresults, name='browseresults'),
     url(r'^orderitem', views.orderitem, name='orderitem'),
+    url(r'^search', views.search, name='search'),
+    url(r'^searchresults', views.searchresults, name='searchresults'),
 ]
