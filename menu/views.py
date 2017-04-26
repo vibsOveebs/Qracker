c1a25051789c3168b2adcd5d4113363971ccd94a
keegan <kzo5060@psu.edu>
2017-04-26 13:19:54 -0400

    fixed search function view formatted menu/views.py renamed results template to searchresults

--- menu/views.py
+++ menu/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-from menu.forms import AddItemForm, BrowseForm, SearchForm, BrowseResultsForm
+from menu.forms import AddItemForm, BrowseForm, SearchForm
 from menu.models import Item
-
-from qracker.views import index
-
 from orders.forms import PartialInitiateForm
-from orders.models import Transaction
-
-from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.models import User
 from django.db.models import Q
 
 
+# add item view
 def add_menu_item(request):
     form = AddItemForm()
 
@@ -26,6 +21,7 @@ def add_menu_item(request):
     return render(request, "menu/add_item.html", {'form': form})
 
 
+# success view
 def success(request):
     return render(request, 'menu/success.html')
 
@@ -34,60 +30,25 @@ def success(request):
 def search(request):
     form = SearchForm()
 
+    # if search form is submitted, go to results
     if request.method == 'POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            return results(request)
-        else:
-            print(form.errors)
+        return searchresults(request)
 
-    return render(request, 'menu/search.html', {'form': form})
+    # else show blank form
+    else:
+        return render(request, 'menu/search.html', {'form': form})
 
 
-# results view
-def results(request):
+# search results view
+def searchresults(request):
     # get search parameters
     searchstring = request.POST.get('searchstring')
-    food_or_drink = request.POST.get('food_or_drink')
-    is_breakfast = request.POST.get('is_breakfast')
-    is_lunch = request.POST.get('is_lunch')
-
-    # filter the Item set using search parameters
-    results = Item.objects.filter(
-        food_or_drink__exact=food_or_drink
-    ).filter(
-        is_breakfast__exact=is_breakfast
-    ).filter(
-        is_lunch__exact=is_lunch
-    ).filter(
-        name__icontains=searchstring
-    ).order_by(
-        '-promo_flag'
-    )
-
-    # ORDER RESULTS BY PROMO FLAG THEN BY POPULARITY
-
-    context_dict = {'results': results}
-
-    # render
-    return render(request, 'menu/results.html', context_dict)
-
-
-def browse(request):
-    form = BrowseForm()
-
-    if request.method == 'POST':
-        return browseresults(request)
-    else:
-        return render(request, 'menu/browse.html', {'form': form})
-
-def browseresults(request):
-    restaurant_name = request.POST.get('restaurant_name')
     is_food = request.POST.get('is_food')
     is_drink = request.POST.get('is_drink')
     is_breakfast = request.POST.get('is_breakfast')
     is_lunch = request.POST.get('is_lunch')
 
+    # get food and drink states
     if (is_food and is_drink):
         FOOD = 'FOOD'
         DRINK = 'DRINK'
@@ -101,37 +62,141 @@ def browseresults(request):
         FOOD = 'FOOD'
         DRINK = 'DRINK'
 
+    # filter and sort for each case
     if (is_breakfast and is_lunch):
-        item_list = Item.objects.filter(supplier=restaurant_name).filter(
-            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK))
+        results = Item.objects.filter(
+            name__icontains=searchstring).filter(
+            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK)).order_by(
+            '-promo_flag'
+        )
     elif (is_breakfast and not is_lunch):
-        item_list = Item.objects.filter(supplier=restaurant_name).filter(is_breakfast=True).filter(
-            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK))
+        results = Item.objects.filter(
+            name__icontains=searchstring).filter(
+            is_breakfast=True).filter(
+            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK)).order_by(
+            '-promo_flag'
+        )
     elif (not is_breakfast and is_lunch):
-        item_list = Item.objects.filter(supplier=restaurant_name).filter(is_lunch=True).filter(
-            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK))
+        results = Item.objects.filter(
+            name__icontains=searchstring).filter(
+            is_lunch=True).filter(
+            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK)).order_by(
+            '-promo_flag'
+        )
     else:
-        item_list = Item.objects.filter(supplier=restaurant_name).filter(
-            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK))
+        results = Item.objects.filter(
+            name__icontains=searchstring).filter(
+            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK)).order_by(
+            '-promo_flag'
+        )
+
+    context_dict = {'results': results}
+
+    # render
+    return render(request, 'menu/searchresults.html', context_dict)
+
+
+# browse items view
+def browse(request):
+    form = BrowseForm()
+
+    # if browse form submitted, go to results
+    if request.method == 'POST':
+        return browseresults(request)
+
+    # else show blank form
+    else:
+        return render(request, 'menu/browse.html', {'form': form})
+
+
+# browse results view
+def browseresults(request):
+    # get browse parameters
+    restaurant_name = request.POST.get('restaurant_name')
+    is_food = request.POST.get('is_food')
+    is_drink = request.POST.get('is_drink')
+    is_breakfast = request.POST.get('is_breakfast')
+    is_lunch = request.POST.get('is_lunch')
+
+    # get food and drink states
+    if (is_food and is_drink):
+        FOOD = 'FOOD'
+        DRINK = 'DRINK'
+    elif (is_food):
+        FOOD = 'FOOD'
+        DRINK = -1
+    elif (is_drink):
+        FOOD = -1
+        DRINK = 'DRINK'
+    else:
+        FOOD = 'FOOD'
+        DRINK = 'DRINK'
+
+    # filter and sort for each case
+    if (is_breakfast and is_lunch):
+        item_list = Item.objects.filter(
+            supplier=restaurant_name).filter(
+            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK)).order_by(
+            '-promo_flag'
+        )
+    elif (is_breakfast and not is_lunch):
+        item_list = Item.objects.filter(
+            supplier=restaurant_name).filter(
+            is_breakfast=True).filter(
+            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK)).order_by(
+            '-promo_flag'
+        )
+    elif (not is_breakfast and is_lunch):
+        item_list = Item.objects.filter(
+            supplier=restaurant_name).filter(
+            is_lunch=True).filter(
+            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK)).order_by(
+            '-promo_flag'
+        )
+    else:
+        item_list = Item.objects.filter(
+            supplier=restaurant_name).filter(
+            Q(food_or_drink=FOOD)|Q(food_or_drink=DRINK)).order_by(
+            '-promo_flag'
+        )
 
     context_dict = {'items': item_list}
 
+    # render
     return render(request, 'menu/browseresults.html', context_dict)
 
+
+# order item view
 def orderitem(request):
+    # get form and currently logged in user
     form = PartialInitiateForm();
     userid = request.user.id
 
+    # if get, pull item id from request
     if request.method == 'GET':
         itemid = request.GET['itemid']
+
+    # if post, submit request
     elif request.method == 'POST':
+        # get itemid from request
         itemid = request.POST.get('itemid')
         form = PartialInitiateForm(request.POST)
+
+        # save form but don't commit
         transaction = form.save(commit=False)
+
+        # put userid and itemid into form
         transaction.initiates = User.objects.get(id=userid)
         transaction.item = Item.objects.get(id=itemid)
+
+        # save form, commit
         transaction.save()
+
+        # print errors
         print(form.errors)
+
+        # show success page
         return success(request)
 
+    # render open form
     return render(request, 'menu/orderitem.html', {'form': form, 'itemid' : itemid})
