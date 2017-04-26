82ea3349e522265428630f0c38b64495a5a1e991
keegan <kzo5060@psu.edu>
2017-04-26 16:22:16 -0400

    orders/forms formatting

--- orders/forms.py
+++ orders/forms.py
@@ -1,14 +1,18 @@
+from datetime import datetime
 from django import forms
 from orders.models import Transaction
-from datetime import datetime
-from userprofile.models import UserProfile
-from django.contrib.auth.models import User
 from random import randint
 
+
+# random code generator
 def generate_code():
     return randint(0,9999)
 
+
+# Partial Initiate Form
 class PartialInitiateForm(forms.ModelForm):
+
+    # item quantity choices
     QUANTITY_CHOICES = (
         (1, '1'), 
         (2, '2'), 
@@ -16,11 +20,14 @@ class PartialInitiateForm(forms.ModelForm):
         (4, '4'), 
         (5, '5'),
     )
+
+    # pickup location choics
     LOC_CHOICES = (
         ('Stairs', 'Stairs'), 
         ('Aquarium', 'Aquarium'), 
         ('Seal', 'Seal'),
     )
+
     creation_time = forms.DateTimeField(initial=datetime.now, widget=forms.HiddenInput())
     pickup_loc = forms.ChoiceField(choices=LOC_CHOICES, help_text="Please select a pickup location: ")
     quantity = forms.ChoiceField(choices=QUANTITY_CHOICES, help_text="Please select a quantity: ")
@@ -30,6 +37,8 @@ class PartialInitiateForm(forms.ModelForm):
         model = Transaction
         fields = ('creation_time', 'pickup_loc', 'quantity', 'code')
 
+
+# Open Orders Form
 class OpenOrdersForm(forms.ModelForm):
     class Meta:
         model=Transaction
