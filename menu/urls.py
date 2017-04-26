51e6038dac92e5356d1ad035b90404e8faf258e3
keegan <kzo5060@psu.edu>
2017-04-26 13:24:56 -0400

    updated menu/urls for search results url

--- menu/urls.py
+++ menu/urls.py
@@ -4,6 +4,7 @@ from menu import views
 urlpatterns = [
     url(r'^additem', views.add_menu_item, name='additem'),
     url(r'^search', views.search, name='search'),
+    url(r'^searchresults', views.searchresults, name='searchresults'),
     url(r'^browse', views.browse, name='browse'),
     url(r'^browseresults', views.browseresults, name='browseresults'),
     #url(r'^orderitem/(?P<itemid>[0-9]*)/?submit=', views.orderitem, name='orderitem'),
